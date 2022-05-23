f7b244f35eed6405b5526be61b5b2cc9441975e7
Edu <Edu@IGLAM-SOLUTIONS>
2022-05-23 13:52:04 -0500

    Agregar archivos de proyecto.

---
+++ Semana6/Consulta.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Consulta : ContentPage
+    {
+        private const string Url = "http://192.168.0.4/moviles/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<Semana6.Datos> _post;
+        public Consulta()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            var content = await client.GetStringAsync(Url);
+            List<Semana6.Datos> posts = JsonConvert.DeserializeObject<List<Semana6.Datos>>(content);
+            _post = new ObservableCollection<Semana6.Datos>(posts);
+            MyListView.ItemsSource = _post;
+        }
+    }
+}
---
+++ Semana6/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            try {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+                cliente.UploadValues("http://192.168.0.4/moviles/post.php", "POST", parametros);
+                DisplayAlert("Mensaje de Alerta", "Registro Almacenado correctamente", "Ok");
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de Alerta", ex.Message, "Ok");
+            }
+        }
+
+        private void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                const string uri = "http://192.168.0.4/moviles/post.php";
+                WebClient cliente = new WebClient();
+                
+                Datos data = new Datos();
+                data.codigo = int.Parse(txtCodigo.Text);
+                data.nombre = txtNombre.Text;
+                data.apellido = txtApellido.Text;
+                data.edad = int.Parse(txtEdad.Text);
+                var content = JsonConvert.SerializeObject(data);
+
+                var parametros = "";
+                parametros = "?codigo=" + txtCodigo.Text + "&nombre=" + txtNombre.Text + "&apellido=" + txtApellido.Text + "&edad=" + txtEdad.Text;
+                var urlPost = new Uri(uri + parametros);
+
+                cliente.UploadString(urlPost, "PUT", content);
+                DisplayAlert("Mensaje de Alerta", "Registro Actualizado correctamente", "Ok");
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de Alerta", ex.Message, "Ok");
+            }
+        }
+
+        private async void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                const string uri = "http://192.168.0.4/moviles/post.php";
+                
+                WebClient cliente = new WebClient();
+                string parametros = "?codigo=" + txtCodigo.Text;
+                var urlPost = new Uri(uri + parametros);
+                
+                cliente.UploadStringAsync(urlPost,"DELETE","");
+                DisplayAlert("Mensaje de Alerta", "Registro Eliminado", "Ok");
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de Alerta", ex.Message, "Ok");
+            }
+        }
+    }
+}
+
